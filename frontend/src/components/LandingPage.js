6f0feaa4276165126eadbc627ff64334e48d8ea2
Jason Paul Darivemula <jasonpauldj@gmail.com>
2022-10-15 22:01:42 -0400

    Add LoginPage

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,38 +1,38 @@
 import classes from './layout/Layout_3_1.module.css';
 import LightButton from './generic/LightButton';
 import DarkButton from './generic/DarkButton';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-function LandingPage(props){
+function LandingPage(props) {
     const navigate = useNavigate();
-    const handleLoginOnClick=()=>{
+    const handleLoginOnClick = () => {
         console.log("IN HANDLE LOG IN")
         navigate('/login');
     }
 
-    return(
+    return (
         <div className={classes.container}>
             <div className={classes.navRow}>
-                <img className={classes.logo} src={require('../assets/Logo.png')}/>
+                <img className={classes.logo} src={require('../assets/Logo.png')} />
             </div>
             <div className={classes.contentRow}>
                 <div className={classes.sideImage}>
-                        <img src={require('../assets/HomeImg.png')} />
+                    <img src={require('../assets/HomeImg.png')} />
                 </div>
                 <div className={classes.sideContent}>
                     <div className={classes.tagLine}>
-                    Flying with strangers won’t be the same anymore, try Floak!!!
+                        Flying with strangers won’t be the same anymore, try Floak!!!
                     </div>
                     <div className={classes.subTagLine}>
-                    With Floak, meet other interesting people who would be travelling with you in air! Network, date, watch movies together! Enter your flight number and get started!
+                        With Floak, meet other interesting people who would be travelling with you in air! Network, date, watch movies together! Enter your flight number and get started!
                     </div>
                     <div className={classes.btnRow}>
                         <LightButton btnText='Login' onClick={handleLoginOnClick} />
                         <DarkButton btnText='Sign Up' />
                     </div>
-                    </div>
                 </div>
             </div>
+        </div>
     )
 }
 
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
+import classes from './Login.module.css';
 import { Link, useNavigate } from "react-router-dom";
 import { Form, Alert } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import { useUserAuth } from "../context/userAuthContext";
+import DarkButton from './generic/DarkButton';
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [error, setError] = useState(null);
   const { logIn } = useUserAuth();
   const navigate = useNavigate();
 
@@ -18,39 +20,62 @@ const Login = () => {
       await logIn(email, password);
       navigate("/home");
     } catch (err) {
-      setError(err.message);
+      setError('Please enter valid user credentials');
     }
   };
 
   return (
     <>
-      <div className="p-4 box">
-        <h2 className="mb-3">Floak Login</h2>
-        {error && <Alert variant="danger">{error}</Alert>}
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control
-              type="email"
-              placeholder="Email address"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
-
-          <div className="d-grid gap-2">
-            <Button variant="primary" type="Submit">
-              Log In
-            </Button>
+      <div className={classes.container}>
+        <div className={classes.navRow}>
+          <img className={classes.logo} src={require('../assets/Logo.png')} />
+        </div>
+        <div className={classes.contentRow}>
+          <div className={classes.sideImage}>
+            <img src={require('../assets/HomeImg.png')} />
           </div>
-        </Form>
-        <hr />
+          <div className={classes.sideContent}>
+            <div className={classes.formContainer}>
+              <div className={classes.message}>
+                Hey there, We missed you!!!
+              </div>
+              <div className={classes.subMessage}>
+                Enter your credentials and start where you left.
+              </div>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label className={classes.formLabel}>Email address</Form.Label>
+                  <Form.Control
+                    className={classes.formInput}
+                    type="email"
+                    placeholder="Email address"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label className={classes.formLabel}>Password</Form.Label>
+                  <Form.Control
+                    className={classes.formInput}
+                    type="password"
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Form.Group>
+                {/* <div className="d-grid gap-2">
+                  <Button variant="primary" type="Submit">
+                    Log In
+                  </Button>
+                </div> */}
+                 {error && <Alert variant="danger">{error}</Alert>}
+                <div className={classes.btnLogin}>
+                  <DarkButton btnText='Login' type="Submit" />
+                </div>
+              </Form>
+            </div>
+
+          </div>
+        </div>
       </div>
       <div className="p-4 box mt-3 text-center">
         Don't have an account? <Link to="/signup">Sign up</Link>
