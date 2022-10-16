85fe3d57ca09eb370bff47884601ce349ad11722
Jason Paul Darivemula <jasonpauldj@gmail.com>
2022-10-15 21:26:39 -0400

    Add Landing Page

---
+++ frontend/src/components/layout/Layout_3_1.js
@@ -0,0 +1,19 @@
+import classes from './Layout_3_1.module.scss';
+
+function Layout_3_1(){
+    return(
+        <div className={classes.container}>
+            <div className={classes.navRow}>
+
+            </div>
+            <div className={classes.contentRow}>
+                <div className={classes.sideImage}>
+
+                </div>
+                <div className={classes.sideContent}>
+
+                </div>
+            </div>
+        </div>
+    )
+}
